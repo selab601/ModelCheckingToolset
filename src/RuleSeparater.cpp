51eb12c2734f956a0740a2d734e8d537decd0986
FLT <15nm726t@vc.ibaraki.ac.jp>
2017-02-10 18:45:50 +0900

    namespaceの変更

--- src/RuleSeparater.cpp
+++ src/RuleSeparater.cpp
@@ -3,19 +3,12 @@
 #include <sstream>
 #include <vector>
 #include <list>
-#include <map>
-#include <set>
+#include <unordered_set>
 #include <string>
-#include <random>
 #include <iterator>
 
 #include <algorithm>
 #include <functional>
-#include <iomanip>
-
-#include <time.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 
 #include <boost/filesystem.hpp>
 #include <boost/algorithm/string.hpp>
@@ -29,14 +22,14 @@
 using namespace boost::spirit;
 
 struct parsedAttributes {
-  std::set<std::string> preAttributes;
-  std::set<std::string> postAttributes;
+  std::unordered_set<std::string> preAttributes;
+  std::unordered_set<std::string> postAttributes;
 };
 
 BOOST_FUSION_ADAPT_STRUCT(
   parsedAttributes,
-  (std::set<std::string>,preAttributes)
-  (std::set<std::string>,postAttributes)
+  (std::unordered_set<std::string>,preAttributes)
+  (std::unordered_set<std::string>,postAttributes)
 );
 
 template<typename Iterator>
@@ -58,13 +51,13 @@ auto parseRule(const std::string& str){
   parsedAttributes result;
   ruleGrammar<std::string::const_iterator> grammar;
   auto it = str.begin();
-  // 入力の全てが文法に従っているか？
   if(qi::phrase_parse(it,str.end(),grammar>>eoi,qi::space,result)==true){
     return result;
   }else {throw "Parse Error.";}
 }
 
 int main(int argc, char const *argv[]){
+  using namespace std;
   try{
     if(argc!=4){
       throw "Usage: ./RuleSeparater [RuleList] [AttributeList] [OutPutDir]";
@@ -86,7 +79,7 @@ int main(int argc, char const *argv[]){
       throw "そんなディレクトリはない";
     }
 
-    std::string resultDirFullPath = resultDirPath.string()+rulePath.stem().string()+"_separated/";
+    string resultDirFullPath = resultDirPath.string()+rulePath.stem().string()+"_separated/";
 
     if(boost::filesystem::exists(resultDirFullPath)){
       resultDirFullPath.insert(0,"分割結果がすでにある : ");
@@ -95,21 +88,21 @@ int main(int argc, char const *argv[]){
 
     boost::filesystem::create_directory(resultDirFullPath);
 
-    std::ifstream ruleList(argv[1]);
+    ifstream ruleList(argv[1]);
     if(ruleList.fail()){
       throw "ルールリスト読込みエラー";
     }
 
-    std::ifstream attrTable(argv[2]);
+    ifstream attrTable(argv[2]);
     if(attrTable.fail()){
       throw "属性テーブル読込みエラー";
     }
 
     // ルールRnの[x]-conditionの条件式
     // conditions[n].first(="pre-condition")
-    std::vector<std::pair<std::set<std::string>,std::set<std::string>>> rules;
+    vector<pair<unordered_set<string>,unordered_set<string>>> rules;
 
-    for(std::string str;std::getline(ruleList,str) && str.size()>0;){
+    for(string str;getline(ruleList,str) && str.size()>0;){
       // 事前事後に分ける
       auto result=parseRule(str);
       rules.emplace_back(make_pair(result.preAttributes,result.postAttributes));
@@ -118,36 +111,36 @@ int main(int argc, char const *argv[]){
     // 「ノード」と、「そのノードから遷移するノードの集合」の組を取得
     // relations[ID].{source:ノード,destinations:次ノード群,commonProperty:共有変数}
     struct relationStructure{
-      std::set<int> source;
-      std::list<std::set<int>> destinations;
+      set<int> source;
+      list<set<int>> destinations;
     };
 
     Eigen::MatrixXd laplacianMatrix=Eigen::MatrixXd::Zero(rules.size(),rules.size());
-    std::vector<relationStructure> relations;
+    vector<relationStructure> relations;
     for(auto rule=rules.begin();rule!=rules.end();++rule){
       relationStructure relation;
-      relation.source.emplace(std::distance(rules.begin(),rule));
+      relation.source.emplace(distance(rules.begin(),rule));
       // rule.post -> destRule.pre==true || destRule.post -> rule.pre==true
       for(auto destRule=rules.begin();destRule!=rules.end();++destRule){
         if(rule==destRule){
           continue;
         }
         // 隣接数A(rule)(destRule)を求める
-        if(std::any_of(rule->second.begin(),rule->second.end(),[&](auto postAttribute){
+        if(any_of(rule->second.begin(),rule->second.end(),[&](auto postAttribute){
           return destRule->first.count(postAttribute)==1;
         })){
-          relation.destinations.emplace_back(std::set<int>({static_cast<int>(std::distance(rules.begin(),destRule))}));
-          laplacianMatrix(static_cast<int>(std::distance(rules.begin(),rule)),static_cast<int>(std::distance(rules.begin(),destRule)))-=1.0;
+          relation.destinations.emplace_back(set<int>({static_cast<int>(distance(rules.begin(),destRule))}));
+          laplacianMatrix(static_cast<int>(distance(rules.begin(),rule)),static_cast<int>(distance(rules.begin(),destRule)))-=1.0;
         }
         // 隣接数B(rule)(destRule)を求める
-        if(std::any_of(rule->first.begin(),rule->first.end(),[&](auto preAttribute){
+        if(any_of(rule->first.begin(),rule->first.end(),[&](auto preAttribute){
           return destRule->second.count(preAttribute)==1;
         })){
-          relation.destinations.emplace_back(std::set<int>({static_cast<int>(std::distance(rules.begin(),destRule))}));
-          laplacianMatrix(static_cast<int>(std::distance(rules.begin(),rule)),static_cast<int>(std::distance(rules.begin(),destRule)))-=1.0;
+          relation.destinations.emplace_back(set<int>({static_cast<int>(distance(rules.begin(),destRule))}));
+          laplacianMatrix(static_cast<int>(distance(rules.begin(),rule)),static_cast<int>(distance(rules.begin(),destRule)))-=1.0;
         }
       }
-      laplacianMatrix(static_cast<int>(std::distance(rules.begin(),rule)),static_cast<int>(std::distance(rules.begin(),rule)))=relation.destinations.size();
+      laplacianMatrix(static_cast<int>(distance(rules.begin(),rule)),static_cast<int>(distance(rules.begin(),rule)))=relation.destinations.size();
       relations.emplace_back(relation);
     }
 
@@ -157,17 +150,17 @@ int main(int argc, char const *argv[]){
     // 固有値をベクトル化
     Eigen::VectorXd m_solved_val = es.eigenvalues().real().cast<double>();
 
-    // さらに探索用のstd::vectorに変換
-    std::vector<double> sortee(m_solved_val.data(), m_solved_val.data() + m_solved_val.rows() * m_solved_val.cols());
+    // さらに探索用のvectorに変換
+    vector<double> sortee(m_solved_val.data(), m_solved_val.data() + m_solved_val.rows() * m_solved_val.cols());
 
     // 一番大きい要素を削除
-    sortee.erase(std::remove(sortee.begin(), sortee.end(), *std::min_element(sortee.begin(),sortee.end())), sortee.end());
+    sortee.erase(remove(sortee.begin(), sortee.end(), *min_element(sortee.begin(),sortee.end())), sortee.end());
 
-    Eigen::VectorXd resultEigen=es.eigenvectors().col(std::distance(
+    Eigen::VectorXd resultEigen=es.eigenvectors().col(distance(
       m_solved_val.data(),
-      std::find(m_solved_val.data(), m_solved_val.data() + m_solved_val.rows() * m_solved_val.cols(),*std::min_element(sortee.begin(),sortee.end()))
+      find(m_solved_val.data(), m_solved_val.data() + m_solved_val.rows() * m_solved_val.cols(),*min_element(sortee.begin(),sortee.end()))
       )).real().cast<double>();
-    std::vector<int> ruleCuts;
+    vector<int> ruleCuts;
 
     int count=0;
     for(auto iter=resultEigen.data();iter!=(resultEigen.data()+resultEigen.rows());++iter,++count){
@@ -177,7 +170,18 @@ int main(int argc, char const *argv[]){
     }
 
     ruleList.clear();
-    ruleList.seekg(0,std::ios::beg);
+    ruleList.seekg(0,ios::beg);
+
+
+    //
+    // 部分モデルの作成(制御ルール定義と、符号化テーブルの1行目)
+    //
+
+    auto extractAttributeSet = [&](string buffer,unordered_set<string>* preProperties,unordered_set<string>* postProperties){
+      auto result=parseRule(buffer);
+      preProperties->insert(result.preAttributes.begin(), result.preAttributes.end());
+      postProperties->insert(result.postAttributes.begin(), result.postAttributes.end());
+    };
 
     // decodeTable_*
     // カンマ区切りの、
@@ -185,26 +189,27 @@ int main(int argc, char const *argv[]){
     // ruleCuts_*のm番目のルールを、全体グラフのルール番号nに置き換えるためのテーブル
     // 2行目:m番目の属性n
     // →AttrTable_*のm番目の属性を、全体に直した時のn番目の属性になおす
-    std::ofstream decodeTable_A(resultDirFullPath+"decodeTable_A",std::ios::trunc);
-    std::ofstream decodeTable_B(resultDirFullPath+"decodeTable_B",std::ios::trunc);
-    std::string decodeStrA,decodeStrB;
-    std::string readingLineBuffer;
+    ofstream decodeTable_A(resultDirFullPath+"decodeTable_A",ios::trunc);
+    ofstream decodeTable_B(resultDirFullPath+"decodeTable_B",ios::trunc);
+    fstream separateResult_A(resultDirFullPath+"ruleCuts_A",ios::in | ios::out | ios::trunc);
+    fstream separateResult_B(resultDirFullPath+"ruleCuts_B",ios::in | ios::out | ios::trunc);
+    string decodeStrA,decodeStrB;
+    string readingLineBuffer;
+    unordered_set<string> preProperties_A={};
+    unordered_set<string> postProperties_A={};
+    unordered_set<string> preProperties_B={};
+    unordered_set<string> postProperties_B={};
 
-    //
-    // 部分モデルの作成(制御ルール定義と、符号化テーブルの1行目)
-    //
-
-    std::fstream separateResult_A(resultDirFullPath+"ruleCuts_A",std::ios::in | std::ios::out | std::ios::trunc);
-    std::fstream separateResult_B(resultDirFullPath+"ruleCuts_B",std::ios::in | std::ios::out | std::ios::trunc);
-    for(int i=0;std::getline(ruleList, readingLineBuffer);i++){
-      if(std::find(ruleCuts.begin(), ruleCuts.end(),i)!=ruleCuts.end()){
-        separateResult_A<<readingLineBuffer<<std::endl;
-        decodeStrA+=std::to_string(i)+",";
+    for(int i=0;getline(ruleList, readingLineBuffer);i++){
+      if(find(ruleCuts.begin(), ruleCuts.end(),i)!=ruleCuts.end()){
+        separateResult_A<<readingLineBuffer<<endl;
+        decodeStrA+=to_string(i)+",";
       }else{
-        separateResult_B<<readingLineBuffer<<std::endl;
-        decodeStrB+=std::to_string(i)+",";
+        separateResult_B<<readingLineBuffer<<endl;
+        decodeStrB+=to_string(i)+",";
       }
     }
+
     decodeStrA.pop_back();
     decodeStrB.pop_back();
     decodeTable_A<<decodeStrA;
@@ -213,44 +218,37 @@ int main(int argc, char const *argv[]){
     //
     // 部分モデルの作成(属性定義と、符号化テーブルの2行目)
     //
-    std::set<std::string> preProperties_A={};
-    std::set<std::string> postProperties_A={};
-    std::set<std::string> preProperties_B={};
-    std::set<std::string> postProperties_B={};
-    auto extractAttributeSet = [&](std::string buffer,std::set<std::string>* preProperties,std::set<std::string>* postProperties){
-      auto result=parseRule(buffer);
-      preProperties->insert(result.preAttributes.begin(), result.preAttributes.end());
-      postProperties->insert(result.postAttributes.begin(), result.postAttributes.end());
-    };
-    separateResult_A.seekg(0,std::ios::beg);
-    separateResult_B.seekg(0,std::ios::beg);
-    while(std::getline(separateResult_A, readingLineBuffer)){
+
+    separateResult_A.seekg(0,ios::beg);
+    separateResult_B.seekg(0,ios::beg);
+
+    while(getline(separateResult_A, readingLineBuffer)){
       extractAttributeSet(readingLineBuffer,&preProperties_A,&postProperties_A);
     }
-    while(std::getline(separateResult_B, readingLineBuffer)){
+    while(getline(separateResult_B, readingLineBuffer)){
       extractAttributeSet(readingLineBuffer,&preProperties_B,&postProperties_B);
     }
 
-    std::ofstream attributesTable_A(resultDirFullPath+"ruleCuts_A_attrTable",std::ios::trunc);
-    std::ofstream attributesTable_B(resultDirFullPath+"ruleCuts_B_attrTable",std::ios::trunc);
-    std::set<std::string> propertiesOfA=preProperties_A;
-    std::set<std::string> propertiesOfB=preProperties_B;
+    ofstream attributesTable_A(resultDirFullPath+"ruleCuts_A_attrTable",ios::trunc);
+    ofstream attributesTable_B(resultDirFullPath+"ruleCuts_B_attrTable",ios::trunc);
+    unordered_set<string> propertiesOfA=preProperties_A;
+    unordered_set<string> propertiesOfB=preProperties_B;
     propertiesOfA.insert(begin(postProperties_A),end(postProperties_A));
     propertiesOfB.insert(begin(postProperties_B),end(postProperties_B));
 
     decodeStrA.clear();
     decodeStrB.clear();
 
-    for(int i=0;std::getline(attrTable,readingLineBuffer);i++){
-      std::string attr=readingLineBuffer.substr(0,readingLineBuffer.find_first_of(","));
+    for(int i=0;getline(attrTable,readingLineBuffer);i++){
+      string attr=readingLineBuffer.substr(0,readingLineBuffer.find_first_of(","));
       // 読み込んだ属性がA側に出てくるなら、A側にコピー
       if(propertiesOfA.count(attr)>0){
-        attributesTable_A<<readingLineBuffer<<std::endl;
-        decodeStrA+=std::to_string(i)+",";
+        attributesTable_A<<readingLineBuffer<<endl;
+        decodeStrA+=to_string(i)+",";
       }
       if(propertiesOfB.count(attr)>0){
-        attributesTable_B<<readingLineBuffer<<std::endl;
-        decodeStrB+=std::to_string(i)+",";
+        attributesTable_B<<readingLineBuffer<<endl;
+        decodeStrB+=to_string(i)+",";
       }
     }
     decodeStrA.pop_back();
@@ -261,36 +259,36 @@ int main(int argc, char const *argv[]){
     //
     // 共通属性定義の作成
     //
-    std::vector<std::string> commonProperties;
-    std::vector<std::string> commonPropertiesAtoB;
-    std::vector<std::string> commonPropertiesBtoA;
+    vector<string> commonProperties;
+    vector<string> commonPropertiesAtoB;
+    vector<string> commonPropertiesBtoA;
 
-    std::set_intersection(preProperties_A.begin(), preProperties_A.end()
+    set_intersection(preProperties_A.begin(), preProperties_A.end()
       , postProperties_B.begin(), postProperties_B.end()
-      , std::back_inserter(commonPropertiesAtoB));
+      , back_inserter(commonPropertiesAtoB));
 
-    std::set_intersection(preProperties_B.begin(), preProperties_B.end()
+    set_intersection(preProperties_B.begin(), preProperties_B.end()
       , postProperties_A.begin(), postProperties_A.end()
-      , std::back_inserter(commonPropertiesBtoA));
+      , back_inserter(commonPropertiesBtoA));
 
-    std::set_intersection(commonPropertiesAtoB.begin(), commonPropertiesAtoB.end()
+    set_intersection(commonPropertiesAtoB.begin(), commonPropertiesAtoB.end()
       , commonPropertiesBtoA.begin(), commonPropertiesBtoA.end()
-      , std::back_inserter(commonProperties));
+      , back_inserter(commonProperties));
 
-    std::string tmp="";
+    string tmp="";
     for(auto i : commonProperties){
       tmp+=i+",";
     }
     if(tmp.size()>0){
       tmp.pop_back();
-      std::ofstream commonPropertiesFile(resultDirFullPath+"commonProperties",std::ios::trunc);
+      ofstream commonPropertiesFile(resultDirFullPath+"commonProperties",ios::trunc);
       commonPropertiesFile<<tmp;
     }
 
-    std::cout<<"Done."<<std::endl;
+    cout<<"Done."<<endl;
     return 0;
   }catch(const char* str){
-    std::cout<<str<<std::endl;
+    cout<<str<<endl;
     return -1;
   }
 }
